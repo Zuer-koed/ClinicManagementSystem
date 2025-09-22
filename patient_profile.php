711f62ac87ceded5c292d70877c23152fbcdd515
p22014061-glitch <p22014061@student.newinti.edu.my>
2025-09-22 15:20:26 +0800

    html for patient profile

---
+++ patient_profile.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patient Dashboard - My Profile</title>
+</head>
+<body>
+    
+    <nav>
+        <ul>
+            <li><a href="patient_dashboard.php">Dashboard</a></li>
+            <li><a href="my_appointments.php">My Appointments</a></li>
+            <li><a href="book_appointment.php">Book Appointment</a></li>
+            <li><a href="my_profile.php">My Profile</a></li>
+            <li><a href="medical_history.php">Medical History</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </nav>
+
+    
+         <h1>My Profile</h1>
+         <main>
+        
+        <section id="profile">
+            <h2>Profile Information</h2>
+            <table border="1" cellpadding="10" cellspacing="0">
+                <tr>
+                    <th>Field</th>
+                    <th>Value</th>
+                </tr>
+                <tr>
+                    <td><strong>Patient ID</strong></td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td><strong>Full Name</strong></td>
+                    <td>Hajimi</td>
+                </tr>
+                <tr>
+                    <td><strong>Date of Birth</strong></td>
+                    <td>March 15, 2068</td>
+                </tr>
+                <tr>
+                    <td><strong>Email Address</strong></td>
+                    <td>hajimi.gmail.com</td>
+                </tr>
+                <tr>
+                    <td><strong>Phone Number</strong></td>
+                    <td>(+60) 10785 6788 <button>Edit</button></td>
+                </tr>
+                <tr>
+                    <td><strong>Address</strong></td>
+                    <td>123,Taman Mambo <button>Edit</button></td>
+                </tr>
+                <tr>
+                    <td><strong>Emergency Contact</strong></td>
+                    <td>Jacky (+60) 10085 66543</td>
+                </tr>
+                <tr>
+                    <td><strong>Blood Type</strong></td>
+                    <td>AB</td>
+                </tr>
+                <tr>
+                    <td><strong>Primary Care Physician</strong></td>
+                    <td>Dr. Hanami</td>
+                </tr>
+            </table>
+        </section>
+    
+    </main>
+
+       <footer>
+    <p>&copy; 2024 City Clinic. All rights reserved.</p>
+  </footer>
+
+</body>
+</html>
