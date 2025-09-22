926f835287d507f3ac15d13ca45fcb42b66b99db
p22014061-glitch <p22014061@student.newinti.edu.my>
2025-09-22 13:34:06 +0800

    HTML for patient dashboard

---
+++ patient_dashboard.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patient Dashboard</title>
+</head>
+<body>
+    
+    <nav>
+        <ul>
+            <li><a href="patient_dashboard.php">Dashboard</a></li>
+            <li><a href="my_appointments.php">My Appointments</a></li>
+            <li><a href="book_appointment.php">Book Appointment</a></li>
+            <li><a href="my_profile.php">My Profile</a></li>
+            <li><a href="medical_history.php">Medical History</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </nav>
+
+    <header>
+        <h1>Welcome,  [Patient Name]!</h1>
+    </header>
+
+    <main>
+        <section>
+            <h2>Next Appointment</h2>
+            <div>
+                <p><strong>Date:</strong> -</p>
+                <p><strong>Time:</strong> -</p>
+                <p><strong>Doctor:</strong> -</p>
+            </div>
+        </section>
+<br>
+        <section>
+            <h2>Quick Stats</h2>
+            <div>
+                <p><strong>Total Appointments:</strong> -</p>
+                <p><strong>Pending Appointments:</strong> -</p>
+            </div>
+        </section>
+    </main>
+
+     <footer>
+    <p>&copy; 2024 City Clinic. All rights reserved.</p>
+  </footer>
+</html>
